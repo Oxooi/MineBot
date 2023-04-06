d3170b53335c0672b91d208ae4d8f7771e69b136
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-04-06 02:22:43 +0200

    add Version

--- config.example.js
+++ config.example.js
@@ -1,6 +1,7 @@
 module.exports = {
     host: 'localhost', // Replace with your server IP
     port: 25565, // Replace with the port of your server
+    version: 1.19, // Replace with your minecraft version
     username: 'ImaBoot', // Rename the bot if u want
     playername: 'YourName' // The name of the bot owner, It's very important
 }
