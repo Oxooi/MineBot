6dbe3a531178e83d5b43bf9f2cfa2ea09cc838b3
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-04-06 02:05:28 +0200

    Optimizing code + add stop follow

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 const mineflayer = require('mineflayer');
-const mcData = require('minecraft-data')('1.19')
+const mcData = require('minecraft-data')('1.19');
 const { FindeMe } = require('./parts/pathfinding/findeme.js');
-const config = require('./settings')
+const { Work } = require('./parts/pathfinding/work.js');
+const config = require('./config');
+// Create a Minecraft bot with the specified IP address and username
+const bot = mineflayer.createBot(config);
 
-// Creation d'un bot Minecraft avec l'adresse IP et le nom d'utilisateur spécifiés
-const bot = mineflayer.createBot(options)
-
-// Création d'une instance FinderMe pour chercher un joueur
+// Create a FinderMe instance to search for players
 const findMe = new FindeMe(bot, mcData);
+const work = new Work(bot);
 
-// Ecoute des messages de chat du joueur
+// Listen for chat messages and handle commands
 bot.on('chat', (username, message) => {
-    // Si le message vient du bot, on ignore
-    if (username === bot.username) return
 
-    switch (message) {
-        // Si le message contient « suis moi » suivi du nom du bot, on suit le joueur spécifié   
-        case bot.username + ' suis moi':
-            findMe.followPlayer(config.playername)
+    // Split the chat message into arguments
+    const args = message.split(' ');
+
+    // Ignore the message if it was sent by the bot itself
+    if (username === bot.username) {
+        return;
+    }
+
+    // Handle commands
+    switch (args[1]) {
+
+        // Mine the specified block a certain number of times
+        case 'mine':
+            const count = args[2];
+            const blockName = args[3];
+            work.GoMineXTimes(blockName, count);
             break;
-        case bot.username + ' coords':
-            findMe.getCoords()
+
+        // Follow a player, default to the sender if no player name is provided
+        case 'follow':
+            const playername = args[2] || username;
+            findMe.followPlayer(playername);
             break;
-        case bot.username + ' distance':
-            findMe.getDistance(config.playername)
+
+        // Stop following any player being followed
+        case 'stop':
+            findMe.stopFollow();
             break;
+
+        // Get the coordinates of the closest player being followed
+        case 'coords':
+            findMe.getCoords();
+            break;
+
+        // Get the distance between the bot and the specified player
+        case 'distance':
+            findMe.getDistance(config.playername);
+            break;
+
+        // If the command is not recognized, do nothing
         default:
             break;
     }
-})
+});
