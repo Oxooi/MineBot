6a338d5d884bccc0d61bc182c6bb3e3c3c5677cd
Oxooi <86341288+Oxooi@users.noreply.github.com>
2023-04-08 01:06:59 +0200

    Optimize & comment the code

--- parts/pathfinding/waypoints.js
+++ parts/pathfinding/waypoints.js
@@ -7,16 +7,16 @@ const yaml = require('js-yaml');
 class Waypoints {
 
     constructor(bot) {
-        this.bot = bot
-        bot.loadPlugin(pathfinder)
+        this.bot = bot;
+        const mcData = require('minecraft-data')(bot.version);
+        this.movements = new Movements(bot, mcData);  // Set up movement options
+        bot.loadPlugin(pathfinder);  // Load pathfinding plugin
     }
 
-    setHome(filePath){
+    // Set the home waypoint and save it to a YAML file
+    setHome(filePath) {
 
-        const posX = this.bot.entity.position.x.toFixed(2)
-        const posY = this.bot.entity.position.y.toFixed(2)
-        const posZ = this.bot.entity.position.z.toFixed(2)
-
+        const { x: posX, y: posY, z: posZ } = this.bot.entity.position.toFixed(2);  // Get current position
         const data = {
             home: {
                 x: posX,
@@ -25,26 +25,25 @@ class Waypoints {
             }
         };
 
-        const yamlString = yaml.dump(data);
-        fs.writeFileSync(filePath, yamlString);
+        const yamlString = yaml.dump(data);  // Convert data to YAML string
+        fs.writeFile(filePath, yamlString, (err) => {  // Write YAML string to file asynchronously
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
 
     }
 
-    getHome(filePath){
-        
-        const mcData = require('minecraft-data')(this.bot.version);  // Get Minecraft data
-        const movements = new Movements(this.bot, mcData);  // Set up movement options
+    // Load the home waypoint from a YAML file and navigate to it using pathfinding
+    getHome(filePath) {
 
-        const fileContents = fs.readFileSync(filePath, 'utf-8');
-        const data = yaml.load(fileContents);
-        const xValue = data.home.x;
-        const yValue = data.home.y;
-        const zValue = data.home.z;
+        const fileContents = fs.readFileSync(filePath, 'utf-8');  // Read YAML file contents
+        const data = yaml.load(fileContents);  // Parse YAML data
+        const { home: { x: xValue, y: yValue, z: zValue } } = data;  // Get x, y, z values for home waypoint
 
-        this.bot.pathfinder.setMovements(movements);
-        const goal = new goals.GoalBlock(xValue, yValue, zValue);
-        this.bot.pathfinder.setGoal(goal, true);
-        
+        this.bot.pathfinder.setMovements(this.movements);  // Set up pathfinding movements
+        const goal = new goals.GoalBlock(xValue, yValue, zValue);  // Set the goal to the home waypoint
+        this.bot.pathfinder.setGoal(goal, true);  // Start pathfinding
+
     }
 
 }
